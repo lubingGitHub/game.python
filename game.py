963d20f4e952ba2dbe4499c97cb30445c962070e
lucoding <2606535225@qq.com>
2018-05-20 11:03:17 +0800

    抽象整体，增加了 ball“

--- game.py
+++ game.py
@@ -3,66 +3,112 @@ from pygame.locals import *
 from sys import exit
 
 
-pygame.init()
+def log(*args, **kw):
+    print(*args, **kw)
 
-screen = pygame.display.set_mode((400, 300))
-pygame.display.set_caption("打砖块")
+def imageFromPath(path):
+    img = pygame.image.load(path).convert_alpha()
+    return img
 
-# background = pygame.image.load(background_image_filename).convert()
-paddle = pygame.image.load('paddle.png').convert_alpha()
-
-x = 100
-y = 250
-speed = 5
-leftDown = False
-rightDown = False
-fps_clock = pygame.time.Clock()
-
-while True:
-    # 游戏主循环
+class Paddle():
+    def __init__(self, x=100, y=250, speed=15):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.image = imageFromPath('paddle.png')
+        self.imageWidth = self.image.get_width()
+        self.imageHeight = self.image.get_height()
 
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            exit()
-        elif event.type == KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                leftDown = True
-            elif event.key == pygame.K_RIGHT:
-                rightDown = True
-        elif event.type == KEYUP:
-                leftDown = False
-                rightDown = False
+    def moveLeft(self):
+        self.x -= self.speed
 
-    # 这里不能放进 for 循坏
-    # key = pygame.key.get_pressed()
-    # if key[pygame.K_LEFT]:
-    #     x -= 10
-    # elif key[pygame.K_RIGHT]:
-    #     x += 10
+    def moveRight(self):
+        self.x += self.speed
+
+    def collode(self, ball):
+        if ball.y + ball.imageHeight > self.y:
+            if ball.x > self.x and ball.x < self.x + self.imageWidth:
+                return True
+
+        return False
+
+class Ball():
+    def __init__(self, x=100, y=200, speed=10):
+        self.x = x
+        self.y = y
+        self.speedX = speed
+        self.speedY = speed
+        self.image = imageFromPath('ball.png')
+        self.fired = False
+        self.imageWidth = self.image.get_width()
+        self.imageHeight = self.image.get_height()
+
+    def move(self):
+        if self.fired:
+            if self.x < 0 or self.x + self.imageWidth> 400:
+                self.speedX *= -1
+            if self.y < 0 or self.y + self.imageHeight> 300:
+                self.speedY *= -1
+
+            self.x += self.speedX
+            self.y += self.speedY
+
+    def fire(self):
+        self.fired = True
 
 
-    if leftDown:
-        x -= speed
-    elif rightDown:
-        x += speed
+class guagame():
+    pygame.init()
+    pygame.display.set_caption("打砖块")
 
-    screen.fill((255,255,255))
-    fps_clock.tick(1000/30)
-    screen.blit(paddle, (x,y))
+    def __init__(self):
+        self.screen = pygame.display.set_mode((400,300))
 
-    pygame.display.update()
+    def clear(self):
+        self.screen.fill((255, 255, 255))
+
+    def draw(self, image):
+        self.screen.blit(image.image, (image.x, image.y))
+
+
+def run():
+    game = guagame()
+    paddle = Paddle()
+    ball = Ball()
+
+    actions = {
+        pygame.K_RIGHT: paddle.moveRight,
+        pygame.K_LEFT: paddle.moveLeft,
+        pygame.K_f: ball.fire
+    }
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
 
 
 
+        key = pygame.key.get_pressed()
+        for k in actions:
+            if key[k]:
+                actions[k]()
 
- # screen.blit(background, (0, 0))
-    # # 将背景图画上去
-    #
-    # x, y = pygame.mouse.get_pos()
-    # # 获得鼠标位置
-    # x -= mouse_cursor.get_width() / 2
-    # y -= mouse_cursor.get_height() / 2
-    # # 计算光标的左上角位置
-    # screen.blit(mouse_cursor, (x, y))
-    # 把光标画上去
+        ball.move()
+
+        if paddle.collode(ball):
+            ball.speedY *= -1
+
+        game.clear()
+        game.draw(paddle)
+        game.draw(ball)
+
+        fps_clock = pygame.time.Clock()
+        fps_clock.tick(1000 / 30)
+
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    run()
